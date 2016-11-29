d03c705cd53f6b862755167283bbfd81d42f0eee
wisnukurniawan <wisnuko13@gmail.com>
2016-11-29 21:19:40 +0700

    initial commit

---
+++ app/src/main/java/com/wisnu_krn/recyclerviewlearn/adapter/MoviesAdapter.java
@@ -0,0 +1,57 @@
+package com.wisnu_krn.recyclerviewlearn.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wisnu_krn.recyclerviewlearn.R;
+import com.wisnu_krn.recyclerviewlearn.model.Movie;
+
+import java.util.List;
+
+/**
+ * Created by private on 29/11/2016.
+ */
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private List<Movie> moviesList;
+
+    public MoviesAdapter(List<Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Movie movie = moviesList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, year, genre;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            genre = (TextView) itemView.findViewById(R.id.genre);
+            year = (TextView) itemView.findViewById(R.id.year);
+        }
+    }
+}
---
+++ app/src/main/java/com/wisnu_krn/recyclerviewlearn/model/Movie.java
@@ -0,0 +1,45 @@
+package com.wisnu_krn.recyclerviewlearn.model;
+
+/**
+ * Created by private on 29/11/2016.
+ */
+
+public class Movie {
+    private String title, genre, year;
+
+    public Movie() {
+
+    }
+
+    public Movie(String title, String genre, String year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+
+}
